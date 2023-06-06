4da4cbb1fd8172877394df8cde2d99fe5e1218d4
Saikumar Reddy Raju <sr19adr@herts.ac.uk>
2023-06-06 16:51:57 +0100

    Spring Java Based Config commit

---
+++ src/main/java/SpringMVC/Employee.java
@@ -0,0 +1,46 @@
+package SpringMVC;
+
+public class Employee {
+    int empid;
+    String empName;
+    double empSalary;
+
+    public Employee(int empid, String empName, double empSalary) {
+        this.empid = empid;
+        this.empName = empName;
+        this.empSalary = empSalary;
+    }
+
+    public int getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(int empid) {
+        this.empid = empid;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getEmpSalary() {
+        return empSalary;
+    }
+
+    public void setEmpSalary(double empSalary) {
+        this.empSalary = empSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empid=" + empid +
+                ", empName='" + empName + '\'' +
+                ", empSalary=" + empSalary +
+                '}';
+    }
+}
--- src/main/java/SpringMVC/HelloController.java
+++ src/main/java/SpringMVC/HelloController.java
@@ -3,14 +3,21 @@ package SpringMVC;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
 
 @Controller
 public class HelloController {
     //To make normal Java method as a Request Handler method use @RequestMapping
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello, Spring MVC!");
-        return "hello"; // view name
+    @RequestMapping(value = "/hello")
+    public String empInfo(Model model) {
+//        model.addAttribute("message", "Hello, Spring MVC!");
+//        return "hello"; // view name
+
+       List<Employee> employeeList = List.of(new Employee(123,"sai",23456),
+                                             new Employee(345,"skr",34213));
+       model.addAttribute("employeeList",employeeList);//model helps in transfer the data to controller to view
+       return "empInfo";
     }
 }
